ff7effb4ffe683e975bd6fdac755af767407ad40
Dave Boyle <dboyle@criticalmass.com>
2015-04-30 16:20:02 -0400

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = require('grunto')(function (grunt) {
+	grunt.loadTasks('tasks');
+
+	grunt.registerTask('default', [
+		'test',
+		'watch'
+	]);
+
+	grunt.registerTask('test', [
+		'eslint',
+		'clean',
+		'excel_vocabulary',
+		'nodeunit'
+	]);
+
+	return {
+		eslint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/*.js',
+				'lib/*.js',
+				'<%= nodeunit.tests %>'
+			]
+		},
+
+		clean: [
+			'.tmp'
+		],
+
+		excel_vocabulary: {
+			convert: {
+				expand: true,
+				cwd: 'examples',
+				dest: '.tmp',
+				ext: '.json',
+				src: [
+					'**/*.xlsx'
+				]
+			}
+		},
+
+		nodeunit: {
+			tests: [ 'tests/*.js' ]
+		},
+
+		watch: {
+			files: [
+				'lib/**/*',
+				'tests/**/*',
+				'tasks/**/*',
+				'examples/**/*'
+			],
+			tasks: [
+				'test'
+			]
+		}
+	};
+});
---
+++ tasks/xlsx_to_json.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var parse = require('./../lib/parse');
+var gruntProcess = require('grunt-process/lib');
+
+module.exports = function (grunt) {
+  grunt.registerMultiTask('xlsx_to_json', function () {
+    var options = this.options({
+      beautify: true,
+      keepEveryRowInFile: false
+    });
+
+    gruntProcess(grunt, this.files, {
+      read: function (src, dest, fileObject) {
+        return parse(src, options);
+      },
+
+      save: function (src, dest, resultJson, fileObject) {
+        var files = {};
+
+        if (options.keepEveryRowInFile) {
+          var count = resultJson.length;
+          var filename;
+
+          for (var i = 0; i < count; i++) {
+            filename = dest.replace(/(.\[a-zA-Z0-9]+)$/, i.toLowerCase() + '$1');
+            files[filename] = JSON.stringify(resultJson[i], null, options.beautify ? 4 : null);
+          }
+        } else {
+          files[dest] = JSON.stringify(resultJson, null, options.beautify ? 4 : null);
+        }
+
+        return files;
+      }
+    }, this.async());
+
+  });
+};
